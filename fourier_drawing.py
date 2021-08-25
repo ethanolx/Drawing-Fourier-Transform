e1cceb988da433be0e75aababd6033e9c8edeb3a
Ethan Tan <imotekhx@gmail.com>
2021-08-25 16:37:21 +0800

    v0.2
     -  Revamped entire design structure
     -  Functions are now methods of the class DrawingFourierTransform
     -  To use them, create a custom class and inherit the above class
     -  3 methods are provided:
        1.  preview (preview the extracted data points)
        2.  draw (use Fourier epicycle to draw 2D outlines)
        3.  morph (transform a circle into 2D shapes)

--- fourier_drawing.py
+++ fourier_drawing.py
@@ -1,12 +1,30 @@
 from manim import *
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Callable, Literal, Union
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from svgpathtools import svg2paths
+from math import pi
 
-def get_complex_coefficients(svg_file: str, N: int = 10, total_num_of_points: int = 100, save_point_plot: str = None, sort_vectors_desc: bool = None):
+
+def preview(X: np.ndarray, Y: np.ndarray, T: np.ndarray, file_save_path: str):
+    sns.scatterplot(
+        x=X,
+        y=Y,
+        hue=T,
+        palette=sns.color_palette('mako', as_cmap=True)
+    )
+    plt.axis('scaled')
+    plt.savefig(file_save_path)
+
+
+def get_points(
+    svg_file: str,
+    total_num_of_points: int = 100,
+    centre_kind: Union[Literal['mass'], Literal['midpoint']] = 'midpoint',
+    flip_vertically: bool = True,
+):
     svg = svg2paths(svg_file_location=svg_file)
 
     PATH = svg[0][0]
@@ -23,180 +41,193 @@ def get_complex_coefficients(svg_file: str, N: int = 10, total_num_of_points: in
     final = []
 
     for p in PATH:
-        num_of_points = round(p.length() / total_length * (total_num_of_points + num_of_paths))
-        final.extend([p.point(t) for t in np.linspace(start=0, stop=1, num=num_of_points)][:-1])
+        num_of_points = round(p.length() / total_length *
+                              (total_num_of_points + num_of_paths))
+        final.extend([p.point(t) for t in np.linspace(
+            start=0, stop=1, num=num_of_points)][:-1])
 
     final_arr = np.array(final)
 
+    # Centre the data points
     X = np.real(final_arr)
-    X = X - X.mean()
+    x_centre = (X.max() + X.min()) / \
+        2 if centre_kind == 'midpoint' else X.mean()
+    X -= x_centre
+
     Y = np.imag(final_arr)
-    Y = Y - Y.mean()
+    y_centre = (Y.max() + Y.min()) / \
+        2 if centre_kind == 'midpoint' else Y.mean()
+    Y -= y_centre
+
+    if flip_vertically:
+        Y *= -1
+
+    # Normalize the data points
     mx = max(X.max(), Y.max())
 
-    X = X / mx
-    Y = Y / mx
+    X /= mx
+    Y /= mx
 
-    min_fr = -int((N - 1) / 2)
-    max_fr = -min_fr if N % 2 == 1 else -min_fr + 1
+    # Generate evenly spaced time intervals
+    T = np.linspace(start=0, stop=2 * pi, num=len(X))
 
-    from math import pi
-    time = np.linspace(start=0, stop=2 * pi, num=len(X))
+    return X, Y, T
 
+
+def get_complex_coefficients(
+    svg_file: str,
+    max_vector_fr: int = 5,
+    total_num_of_points: int = 100,
+    centre_kind: Union[Literal['mass'], Literal['midpoint']] = 'midpoint',
+    save_point_plot: str = None,
+    vector_sort_fn: Union[str, Callable[[
+        pd.DataFrame], pd.Series]] = 'Magnitude',
+    vector_sort_order: Union[Literal['asc'], Literal['desc']] = 'desc'
+):
+    # Retrieve data points
+    X, Y, T = get_points(
+        svg_file=svg_file,
+        total_num_of_points=total_num_of_points,
+        centre_kind=centre_kind
+    )
+
+    # Preview the data points
+    if save_point_plot is not None:
+        preview(X=X, Y=Y, T=T, file_save_path=save_point_plot)
+
+    # Numerical Integration
     s = []
-    for f in range(min_fr, max_fr + 1):
-        s.append((f, sum([(X[i] + Y[i] * 1j) * np.exp(f * 1j * -1 * time[i]) * (2 * pi / len(time)) for i in range(len(X))]) / (2 * pi)))
+    for f in range(-max_vector_fr, max_vector_fr + 1):
+        s.append((f, sum([(X[i] + Y[i] * 1j) * np.exp(f * 1j * -1 * T[i])
+                          * (2 * pi / len(T)) for i in range(len(X))]) / (2 * pi)))
 
-    freqs = np.array([i[0] for i in s])
-    cs = np.array([[i[1].real, i[1].imag] for i in s])
-
-    coefs = np.hstack((cs, freqs.reshape(-1, 1)))
-
+    # Build the dataframe
+    coefs = np.hstack((
+        np.array([[i[1].real, i[1].imag] for i in s]),
+        np.array([i[0] for i in s]).reshape(-1, 1)
+    ))
     df = pd.DataFrame(data=coefs, columns=['Re', 'Im', 'Freq'])
 
-    if save_point_plot is not None:
-        sns.scatterplot(x=X, y=Y, hue=time, palette=sns.color_palette('mako', as_cmap=True))
-        plt.axis('scaled')
-        plt.savefig(save_point_plot)
-
-    if sort_vectors_desc is None:
-        return df.values
-    else:
-        df['Magnitude'] = np.sqrt(df['Re'] ** 2 + df['Im'] ** 2)
-        return df.sort_values(by='Magnitude', ascending=(not sort_vectors_desc)).drop(columns='Magnitude').values
-
-class Draw(Scene):
-
-    SETTINGS: Dict[str, Any] = {
-        'file_path': './img/cowboy.svg',
-        'n_vectors': 401,
-        'n_points': 80000,
-        'vector_scale': 2.5,
-        'cycles_per_s': 1 / 10,
-        'run_time_s': 11,
-        'plot_save_path': './img/cowboy.png'
-    }
-
-    DEFAULT_SETTINGS: Dict[str, Any] = {
-        'file_path': None,
-        'n_vectors': 11,
-        'n_points': 100,
-        'plot_save_path': None,
-        'run_time_s': 10,
-        'vector_scale': 1,
-        'cycles_per_s': 1,
-        'sort_vectors_desc': True
-    }
-
-    def get_settings(self):
-        CONFIG: Dict[str, Any] = {}
-        for k in self.DEFAULT_SETTINGS:
-            if k in self.SETTINGS.keys():
-                CONFIG[k] = self.SETTINGS[k]
+    # Return the sorted dataframe
+    if vector_sort_fn is not None:
+        if type(vector_sort_fn) is str:
+            if vector_sort_fn == 'Magnitude':
+                df['sort_sequence'] = np.sqrt(df['Re'] ** 2 + df['Im'] ** 2)
+            elif vector_sort_fn == 'Frequency':
+                df['sort_sequence'] = np.abs(df['Freq'])
             else:
-                CONFIG[k] = self.DEFAULT_SETTINGS[k]
-        return CONFIG
+                raise NotImplementedError
+        else:
+            df['sort_sequence'] = vector_sort_fn(df)  # type: ignore
+        return df.sort_values(by='sort_sequence', ascending=(vector_sort_order == 'asc')).drop(columns='sort_sequence').values
+    else:
+        return df.values
 
-    def construct(self):
-        CONFIG = self.get_settings()
 
-        coefs = get_complex_coefficients(svg_file=CONFIG['file_path'], N=CONFIG['n_vectors'], total_num_of_points=CONFIG['n_points'], save_point_plot=CONFIG['plot_save_path'], sort_vectors_desc=CONFIG['sort_vectors_desc'])
+class DrawingFourierTransform(Scene):
+    def preview(
+        self,
+        svg_file: str,
+        save_plot_to: str,
+        total_num_of_points: int = 1000,
+        centre_kind: Union[Literal['mass'], Literal['midpoint']] = 'midpoint'
+    ):
+        X, Y, T = get_points(
+            svg_file=svg_file,
+            total_num_of_points=total_num_of_points,
+            centre_kind=centre_kind
+        )
+        preview(X=X, Y=Y, T=T, file_save_path=save_plot_to)
+
+    def draw(
+        self,
+        svg_file: str,
+        max_vector_fr: int = 5,
+        total_num_of_points: int = 1000,
+        save_point_plot: str = None,
+        cycles_per_s: float = 1.0,
+        vector_scale: float = 2.0,
+        run_time_s: float = 10.0,
+        position: np.ndarray = ORIGIN,
+        **kwargs
+    ):
+        coefs = get_complex_coefficients(
+            svg_file=svg_file,
+            max_vector_fr=max_vector_fr,
+            total_num_of_points=total_num_of_points,
+            save_point_plot=save_point_plot,
+            **kwargs
+        )
 
         tracker = ValueTracker()
 
-        vecs: List[Vector] = [Vector(direction=[c[0], c[1]]) for c in coefs] # type:ignore
+        vecs: List[Vector] = [Vector(direction=[c[0], c[1]])
+                              for c in coefs]  # type: ignore
 
-        vecs[0].add_updater(lambda x: x.rotate(angle=coefs[0 ,2] * tracker.get_value(), about_point=x.get_start())) # type:ignore
+        vecs[0].shift(position)
+        vecs[0].add_updater(lambda x: x.rotate(
+            angle=coefs[0, 2] * tracker.get_value(), about_point=x.get_start()))  # type:ignore
 
         for i in range(1, len(vecs)):
-            vecs[i].add_updater(lambda v, i=i: v.move_to(vecs[i - 1].get_end() + v.get_center() - v.get_start()).rotate(angle=coefs[i, 2] * tracker.get_value(), about_point=v.get_start())) # type:ignore
+            vecs[i].add_updater(lambda v, i=i: v.move_to(vecs[i - 1].get_end() + v.get_center() - v.get_start(
+            )).rotate(angle=coefs[i, 2] * tracker.get_value(), about_point=v.get_start()))  # type:ignore
 
         trace = TracedPath(vecs[-1].get_end)
 
-        freq = CONFIG['cycles_per_s']
-        # self.play(Write(Text('Writing the letter E').shift(3 * LEFT + 2 * UP)))
+        freq = cycles_per_s
 
-        self.add(*[v.scale(CONFIG['vector_scale']) for v in vecs], trace)
+        self.add(*[v.scale(vector_scale) for v in vecs], trace)
         tracker.increment_value(PI / 30 * freq)
-        self.play(*[v.animate for v in vecs], run_time=CONFIG['run_time_s'])
+        self.play(*[v.animate for v in vecs], run_time=run_time_s)
 
-class Morph(Scene):
-    SETTINGS: Dict[str, Any] = {
-        'file_path': './img/e.svg',
-        'n_vectors': 50,
-        'n_points': 10000,
-        'vector_scale': 3,
-        'run_time_s': 11,
-        'n_vectors_interval': 1,
-        'plot_save_path': './img/cowboy.png'
-    }
+    def morph(
+        self,
+        svg_file: str,
+        max_vector_fr: int = 5,
+        total_num_of_points: int = 1000,
+        save_point_plot: str = None,
+        delay_btw_transformations_s: float = 0.1,
+        delay_each_transformation_s: float = 0.1,
+        vector_scale: float = 2.0,
+        position: np.ndarray = ORIGIN,
+        n_vectors_interval: int = 1,
+        outline_colour: np.ndarray = WHITE,
+        **kwargs
+    ):
 
-    DEFAULT_SETTINGS: Dict[str, Any] = {
-        'file_path': None,
-        'n_vectors': 11,
-        'n_points': 100,
-        'plot_save_path': None,
-        'run_time_s': 10,
-        'vector_scale': 1,
-        'n_vectors_interval': 1,
-        'sort_vectors_desc': True
-    }
-
-    def get_settings(self):
-        CONFIG: Dict[str, Any] = {}
-        for k in self.DEFAULT_SETTINGS:
-            if k in self.SETTINGS.keys():
-                CONFIG[k] = self.SETTINGS[k]
-            else:
-                CONFIG[k] = self.DEFAULT_SETTINGS[k]
-        return CONFIG
-
-    def construct(self):
-        CONFIG = self.get_settings()
-
-        scale = CONFIG['vector_scale']
         axes = Axes(
             x_range=[-1, 1, 1],
-            x_length=2 * scale,
+            x_length=2 * vector_scale,
             y_range=[-1, 1, 1],
-            y_length=2 * scale
+            y_length=2 * vector_scale
         )
 
-        coefs = get_complex_coefficients(svg_file=CONFIG['file_path'], N=CONFIG['n_vectors'], total_num_of_points=CONFIG['n_points'], save_point_plot=CONFIG['plot_save_path'], sort_vectors_desc=CONFIG['sort_vectors_desc'])
+        coefs = get_complex_coefficients(
+            svg_file=svg_file,
+            total_num_of_points=total_num_of_points,
+            max_vector_fr=max_vector_fr,
+            save_point_plot=save_point_plot,
+            **kwargs
+        )
 
         freqs = []
 
-        # print(coefs)
-
         for i in range(coefs.shape[0]):
-            freqs.append(lambda x, i=i: (coefs[i, 0] + coefs[i, 1] * 1j) * np.exp(coefs[i, 2] * x * 1j))
-
-        # graph = axes.get_parametric_curve(lambda t: np.array([
-        #     np.real([freqs[0](t)]).sum(),
-        #     np.imag([freqs[0](t)]).sum()
-        # ]), t_range=[0, 2 * PI])
-
-        # graph2 = axes.get_parametric_curve(lambda t: np.array([
-        #     np.real([freqs[0](t), freqs[1](t)]).sum(),
-        #     np.imag([freqs[0](t), freqs[1](t)]).sum()
-        # ]), t_range=[0, 2 * PI])
+            freqs.append(lambda x, i=i: (
+                coefs[i, 0] + coefs[i, 1] * 1j) * np.exp(coefs[i, 2] * x * 1j))
 
         graphs = []
 
         for i in range(len(freqs)):
             graphs.append(axes.get_parametric_curve(lambda t, i=i: np.array([
-            np.real([f(t) for f in freqs[:i+1]]).sum(),
-            np.imag([f(t) for f in freqs[:i+1]]).sum()
-        ]), t_range=[0, 2 * PI]))
+                np.real([f(t) for f in freqs[:i+1]]).sum(),
+                np.imag([f(t) for f in freqs[:i+1]]).sum()
+            ]), t_range=[0, 2 * PI], color=outline_colour).shift(position))
 
         curr_graph = graphs[0]
-        curr_num = Text(str(1)).shift(4 * RIGHT + 3 * UP)
         self.add(curr_graph)
 
-        for i, g in enumerate(graphs[1:]):
-            # self.wait(duration=0.1)
-            self.play(Transform(curr_num, Text(str(i + 2)).shift(4 * RIGHT + 3 * UP)), Transform(curr_graph, g), run_time=1/5)
-
-        # self.wait(duration=1)
-        # self.play(Transform(graph, graph2))
-        # self.wait(duration=1)
+        for i, g in enumerate(graphs[1::n_vectors_interval]):
+            self.wait(duration=delay_btw_transformations_s)
+            self.play(Transform(curr_graph, g),
+                      run_time=delay_each_transformation_s)
